9c922ce742d884cd45c28b300a1b442401dd1b05
muzuka <seanbrown7935@gmail.com>
2014-11-15 15:54:56 -0700

    displays messages in inbox

--- inbox.php
+++ inbox.php
@@ -4,6 +4,33 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
+<?php
+
+    include "dataConnect.php";
+    
+    session_start();
+    //Check whether the session variable SESS_USER_ID is present or not
+    if(!isset($_SESSION['sess_user_id']) || (trim($_SESSION['sess_user_id']) === '')) {
+        header("location: loginPage.php");
+        exit();
+    }
+    
+    $userID = $_SESSION['sess_user_id'];
+    
+    $query = "SELECT Subject, TStamp, Viewed FROM Messages WHERE rID = $userID";
+    
+    $result = mysqli_query($conn, $query);
+    
+    if($result) {
+        $numOfMessages = mysqli_num_rows($result);
+    }
+    else {
+        $numOfMessages = 0;
+    }
+
+?>
+
 <html>
     <head>
         <title>TInbox</title>
@@ -37,13 +64,37 @@ and open the template in the editor.
                 <div style="background-color: lightgray; color:black; margin: 20px; padding: 20px">
                     <body>
                         <table>
-                            <tr>
+                            
+                            <?php
+                            
+                                if($numOfMessages == 0) {
+                                    echo "You have no messages.";
+                                }
+                            
+                                for($i = 0; $i < $numOfMessages; $i++) {
+                                    
+                                    $row = mysqli_fetch_assoc($result);
+                                    
+                                    $timeStamp = $row["TStamp"];
+                                    $subject = $row["Subject"];
+                                    $viewed = $row["Viewed"];
+                                    
+                                    echo "<tr><br/>";
+                                    echo "<td> $subject </td>";
+                                    echo "<td> $timeStamp</td>";
+                                    echo "</tr>";
+                                }
+                            
+                            ?>
+                            
+                            
+                            <!--<tr>
                                 <td><a href="WriteaMessage.php" target="_self">user who sent message (note: this is a link, it will go to a page that will display the message)</a></td>
                             <td>date message was sent</td>
                             </tr>
                             <tr>
                                 <td> as many rows as messages.</td>
-                            </tr>
+                            </tr> -->
                         </table>
                     </body>
                 </div>
@@ -51,3 +102,5 @@ and open the template in the editor.
         </div>
     </body>
 </html>
+
+<?php mysqli_close($conn); ?>
