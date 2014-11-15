e329d0aafe95daece28fa01deee1acfc77a15c06
muzuka <seanbrown7935@gmail.com>
2014-11-15 16:56:21 -0700

    displays all book owner information

--- bookPage.php
+++ bookPage.php
@@ -5,17 +5,35 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <?php
+
+    session_start();
+    //Check whether the session variable SESS_USER_ID is present or not
+    if(!isset($_SESSION['sess_user_id']) || (trim($_SESSION['sess_user_id']) === '')) {
+        header("location: loginPage.php");
+        exit();
+    }
     
     include "dataConnect.php";
     
     $bID = $_GET["id"];
     
-    $query = "SELECT Title, Author FROM Book WHERE bID = '$bID'";
+    $bookQuery = "SELECT * FROM Book WHERE bID = '$bID'";
+    $bookResult = mysqli_query($conn, $bookQuery);
     
-    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($bookResult);
     
-    $row = mysqli_fetch_assoc($result);
-    $title = $row["Title"];
+    $owner   = $row["oID"];
+    $title   = $row["Title"];
+    $author  = $row["Author"];
+    $quality = $row["Quality"];
+    
+    $userQuery = "SELECT * FROM User WHERE UserID = '$owner';";
+    $userResult = mysqli_query($conn, $userQuery);
+    
+    $row = mysqli_fetch_assoc($userResult);
+    
+    $username = $row["Username"];
+    $email    = $row["eMail"];
 
 ?>
 
@@ -25,14 +43,14 @@ and open the template in the editor.
         <meta charset="windows-1252">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <div style=" text-align: right; text-decoration-color: blue">
-            <a href="userpage.php"> [return to user page]</a> <a>[log out]</a>
+            <a href="userpage.php"> [return to user page]</a> <a >[log out]</a>
         </div>
     </head>
 
     
     <body>
         <div style="background-color: beige; color:black; margin: 20px; padding: 20px">
-            <h1> <?php echo "$title"; ?> </h1>
+            <h1> <?php echo $title; ?> </h1>
                 <div style="background-color:blue; color:white; margin:10px; padding:5px;text-align: center">
                     <p></p>
                 </div>
@@ -40,15 +58,27 @@ and open the template in the editor.
                 this will be the jpeg
             </div>  
             <p></p>
-            <div>this table needs php</div>
-            <table>
+            <div>Book information</div>
+            <table style="padding: 10px">
                 <tr>
-                    <td>Book Name</td>
-                    <td> "actual book name"</td>
+                    <td>Owner:</td>
+                    <td><?php echo $username; ?></td>
                 </tr>
                 <tr>
-                    <td>Author</td>
-                    <td>"author name"</td>
+                    <td>Email:</td>
+                    <td><?php echo $email; ?></td>
+                </tr>
+                <tr>
+                    <td>Book Title:</td>
+                    <td><?php echo $title; ?></td>
+                </tr>
+                <tr>
+                    <td>Author:</td>
+                    <td><?php echo $author; ?></td>
+                </tr>
+                <tr>
+                    <td>Condition:</td>
+                    <td><?php echo $quality; ?></td>
                 </tr>
             </table>
        </div>
