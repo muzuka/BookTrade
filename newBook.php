9be2bb16c82599ad2a913406f1976d1be4079535
lucid <lucid@VoxBook>
2014-11-15 17:28:20 -0700

    Do not try uploading an image with a new book yet, it is not working...

--- newBook.php
+++ newBook.php
@@ -11,16 +11,24 @@ include 'dataConnect.php';
 
 $title = $_POST['title'];
 $author = $_POST['author'];
-$quality = $POST['quality'];
+$quality = $_POST['quality'];
 $description = $_POST['description'];
 $oid = $_SESSION['sess_user_id'];
 $nbid = mysqli_query($conn, "SELECT UUID()");
+$row = mysqli_fetch_assoc($nbid);
+$nuid = $row['UUID()'];
 
-$insert = "INSERT INTO Book (bID, Title, Author, Quality, oID) VALUES '$nbid', '$title', '$author', '$quality', '$oid';";
-if(mysqli_query($conn, $insert) == TRUE)
+$newBook = "INSERT INTO Book (bID, Title, Author, Quality, oID) VALUES ('$nuid', '$title', '$author', '$quality', '$oid');";
+if ($conn->query($newBook) === TRUE) {
+    header('Location: userPage.php');
+} else {
+    echo "Error: " . $newBook . "<br>" . $conn->error;
+} 
+
+if(isset($description) && trim($description)!='')
 {
-    $desin = "INSERT INTO Description (bID, Description) VALUES '$nbid', '$description'";
-    mysql_query($conn, $desin);
+    $desin = "INSERT INTO Description (bID, Description) VALUES '$nuid', '$description'";
+    mysqli_query($conn, $desin);
 }
 
 // DO NOT UPLOAD AN IMAGE YET - It doesn't work
@@ -36,7 +44,7 @@ if(isset($_FILES['picture']) && trim(isset($_POST['ptext'])))
             $maxsize = 16777215;
             if($image < $maxsize)
             {
-                $pins = "INSERT INTO Picture (bID, pText, Picture) VALUES '$nbid', '$ptxt', '$image';";
+                $pins = "INSERT INTO Picture (bID, pText, Picture) VALUES '$nuid', '$ptxt', '$image';";
                 mysqli_query($conn, $pins);
             }     
         }
