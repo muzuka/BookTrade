856f296832321bb2d761c1363e523e40aa2545ad
muzuka <seanbrown7935@gmail.com>
2014-11-17 12:34:55 -0700

    removed placeholder text

--- browse2.php
+++ browse2.php
@@ -16,28 +16,18 @@ and open the template in the editor.
     
 
     <body>
-    <div style="background-color: beige; color:black; margin: 20px; padding: 20px">
-        <div>
-            <h1>Search </h1>
-            <div style="background-color:blue; color:white; margin:10px; padding:5px;text-align: center">
-                <p></p>
+        <div style="background-color: beige; color:black; margin: 20px; padding: 20px">
+            <div>
+                <h1>Search </h1>
+                <div style="background-color:blue; color:white; margin:10px; padding:5px;text-align: center">
+                    <p></p>
+                </div>
+                <form method="post" action="browse2.php">
+                <p><input type="search" value="search" name="search">
+                    <input type="submit" value="submit" name="Search"></p>
+                </form>
+                <?php include("search.php") ?>
             </div>
-            <form method="post" action="browse2.php">
-            <p><input type="search" value="search" name="search">
-                <input type="submit" value="submit" name="Search"></p>
-            </form>
-            <?php include("search.php") ?>
         </div>
     </body>
-    
-    <table>
-        <tr>
-            <td> book jpeg</td>
-        </tr>
-        <tr>
-            <td> <a href="bookPage.php" target="_self">book title</a> </td>
-        </tr>
-        <tr><a href="Userpagenotself.php" target="_self"> user name</a></tr>
-    </table>
-    </div>
 </html>
