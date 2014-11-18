af3722647532378d2e52dc7757f5c7fd10c95fe8
muzuka <seanbrown7935@gmail.com>
2014-11-17 17:34:13 -0700

    saves message

---
+++ send.php
@@ -0,0 +1,29 @@
+<?php
+
+include "dataConnect.php";
+session_start();
+//Check whether the session variable SESS_USER_ID is present or not
+if(!isset($_SESSION['sess_user_id']) || (trim($_SESSION['sess_user_id']) === '')) {
+    header("location: loginPage.php");
+exit();
+}
+
+
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+if($_POST["submit"] == "delete") {
+    header("Location: inbox.php");
+}
+else {
+    $recID = $_SESSION["pageUser"];
+    $sendID = $_SESSION["sess_user_id"];
+    
+    $date = date("Y-m-d h:i:s");
+    
+    $query = "INSERT INTO Messages(mBody, rID, sID, Subject, TStamp, Viewed) VALUES ('$message', '$recID', '$sendID', '$subject', '$date', 0);";
+    $result = mysqli_query($conn, $query);
+    
+    mysqli_close($conn);
+    header("Location: inbox.php");
+}
