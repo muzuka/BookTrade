f14ee650ce28430c59908c4c91af280135bfcfde
muzuka <seanbrown7935@gmail.com>
2014-11-25 12:28:25 -0700

    now only links to login and register

--- index.php
+++ index.php
@@ -29,93 +29,33 @@ and open the template in the editor.
                 padding:5px;
                 text-align: center;
             }
-            #login
-            {
-                margin-right: 10px;
-                margin-left: 20%;
-                background-color: lightgrey;
-                float: left;
-                width: 30%;
-            }
-            #register
-            {
-                margin-left: 10px;
-                background-color: lightgrey;
-                float: left;
-                width: 30%;
-            }
         </style>
     </head>
     
     <body>
-        <?php
-        // MySQL Connection
-        session_start();
-        include 'dataConnect.php';
-
-        $un = filter_input(INPUT_POST, 'username');
-        $em = filter_input(INPUT_POST, 'email');
-        $pw = filter_input(INPUT_POST, 'password');
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $newUser = "INSERT INTO User(Username, eMail, Pword) VALUES ('$un', '$em', '$pw');";
-
-            if ($conn->query($newUser) === TRUE) {
-                $_SESSION['sess_new'] = 1;
-                header('Location: loginPage.php');
-            } else {
-                echo "Error: " . $newUser . "<br>" . $conn->error;
-            } 
-        }
-
-        $conn->close();
-
-        ?>
         <div id="background">
             <div>
                 <h1>Welcome to the Book Lender </h1>
                 <div id="bar">
-                    <form method="link" action="browse.php" align="left">
-                        <input type="submit" name="submit" value="browse"/>
-                    </form>
-                </div>
-                <div id="login">
-                    <h2 align="center"> Log in </h2>
-                    <form id="loginForm" name="lform" method="post" action="login.php">
-                        <table width="350px" border="0" align="left">
-                            <tr>
-                                <td>Username:</td>
-                                <td><input type="text" name="username" id="username" /></td>
-                            </tr>
-                            <tr>
-                                <td>Password:</td>
-                                <td><input type="password" name="password" id="password" /></td>
-                            </tr>
-                            <tr>
-                                <td>&nbsp;</td>
-                                <td><input type="submit" name="button" id="button" value="Submit" /></td>
-                            </tr>
-                        </table>
-                    </form>
-                </div>
-                <div id="register">
-                    <h2 align="center"> Register </h2>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-                        <table width="350px" border="0px">
-                            <tr>
-                                <td>Username: </td>
-                                <td><input type="text" name="username" value="" /></td>
-                            </tr>
-                            <tr>
-                                <td>E-Mail: </td>
-                                <td><input type="email" name="email" value="" /></td>
-                            </tr>
-                            <tr>
-                                <td>Password: </td>
-                                <td><input type="password" name="password" value="" /></td>
-                            </tr>
-                        </table>
-                        <input type="submit" name="submit" value="Register"/>
-                    </form>
+                    <table>
+                        <tr>
+                           <td>
+                            <form method="link" action="browse.php">
+                                <input type="submit" name="submit" value="browse"/>
+                            </form>
+                           </td> 
+                           <td>
+                               <form method="link" action="loginPage.php">
+                                   <input type="submit" name="submit" value="login"/>
+                               </form>
+                           </td>
+                           <td>
+                               <form method="link" action="register.php">
+                                   <input type="submit" name="submit" value="register"/>
+                               </form>
+                           </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
